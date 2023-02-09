b5c486d83ec2a8caed76c3183b7c6faab2146510
Dayasagar543 <dayasagar.543@gmail.com>
2023-02-09 22:13:21 +0530

    commiting severfile

--- 12-server.js
+++ 12-server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
-http.createServer((req,res) => {
-    if () {
-        
+const server=http.createServer((req,res) => {
+    if (req.url==='/') {
+        res.end("welcome to the home page")
+    }
+    if (req.url === '/about') {
+        res.end("this is our little history ")
     }
 })
+
+server.listen(5000);
